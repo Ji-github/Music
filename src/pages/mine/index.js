31e62f46505ebb62dc75b8d2d9737403fa042e6b
Ji-github <861813667@qq.com>
2021-03-05 16:34:55 +0800

    1

---
+++ src/pages/mine/index.js
@@ -0,0 +1,17 @@
+import React, { memo } from 'react'
+import { NotLogin } from './style'
+
+export default memo(function JMMine() {
+  let isLogin = false
+  return (
+    <div>
+      <NotLogin isLogin={isLogin} className="w980">
+        <div className="show-no-login">
+          <div className="my_music inner" >
+            <h2>登录网易云音乐</h2>
+          </div>
+        </div>
+      </NotLogin>
+    </div>
+  )
+})
